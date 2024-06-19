38368bfdc3bb0652012c73bc5047de605fe99b31
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2024-06-19 20:41:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const inpButton = document.querySelector("input");
+
+inpButton.addEventListener("click", function() {
+	const selectElement = document.getElementById("colorSelect");
+	const selectedIndex = selectElement.selectedIndex;
+	if(selectedIndex === -1) return; 
+	
+	selectElement.remove(selectedIndex);
+})
